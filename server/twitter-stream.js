f36f3514c5e8d0a666fc72bb255c9fe0ccd36635
whiteh <greenwma@googlemail.com>
2016-05-21 15:13:30 +0000

    added nlp_compromise + simple tagging.  Need to move nlp from global ns

--- server/twitter-stream.js
+++ server/twitter-stream.js
@@ -1,7 +1,13 @@
+var nlp;
 if (Meteor.isServer) {
 
   Meteor.methods({
     handleTweet: function(data) {
+      nlp.text(data.text)
+         .sentences
+         .forEach(function(sentence){
+            console.log(sentence.tags());
+         });
       console.log(data.text);
     }
   });
@@ -10,12 +16,14 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     var conf = JSON.parse(Assets.getText("twitter.json"));
     var Twit = Meteor.npmRequire('twit');
+    nlp  = Meteor.npmRequire('nlp_compromise');
     var T = new Twit({
         consumer_key: conf.consumer.key,
         consumer_secret: conf.consumer.secret,
         access_token: conf.access_token.key,
         access_token_secret: conf.access_token.secret
     });
+
     var stream =  T.stream("statuses/sample",
               {
                 "lang" : "en-gb"
