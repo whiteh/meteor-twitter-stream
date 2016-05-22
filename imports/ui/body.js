7bc00cbbb256cf5fa9acaa2cd6c482ebb370b0f5
whiteh <greenwma@googlemail.com>
2016-05-22 21:44:35 +0000

    Timeline grabbing/collections work.  Beginning of stylesheet

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,9 +3,11 @@ import { Template } from 'meteor/templating';
 import { Tweets } from '../api/tweets.js';
 
 import './body.html';
+import './body.css';
 
 Template.body.helpers({
   tweets() {
+    console.log(Tweets.findOne());
     return Tweets.find({});
   },
 });
--- server/twitter-stream.js
+++ server/twitter-stream.js
@@ -1,6 +1,6 @@
-var nlp;
+import { Tweets } from '../imports/api/tweets.js';
+var nlp, Twit, conf;
 if (Meteor.isServer) {
-
   Meteor.methods({
     handleTweet: function(data) {
       nlp.text(data.text)
@@ -9,27 +9,65 @@ if (Meteor.isServer) {
             console.log(sentence.tags());
          });
       console.log(data.text);
+    },
+    getAccessToken: function() {
+      try {
+        return Meteor.user().services.twitter;
+      } catch(e) {
+        return null;
+      }
+    },
+    getTimeline: function() {
+      var access_token = Meteor.user().services.twitter.accessToken;
+      var access_token_secret = Meteor.user().services.twitter.accessTokenSecret;
+
+      var insertTweet = Meteor.bindEnvironment(function(data){
+        Tweets.insert(data);
+      });
+
+      var T = new Twit({
+        consumer_key: conf.consumer.key,
+        consumer_secret: conf.consumer.secret,
+        access_token: access_token,
+        access_token_secret: access_token_secret
+      });
+      test = T.get('statuses/home_timeline', { screen_name: "whitehankey"  })
+        .catch(function (err) {
+          console.log('caught error', err.stack)
+        })
+        .then(function (result) {
+          console.log(result.data.length + " tweets received");
+          result.data.forEach(function(data){
+            insertTweet(data);
+          })
+        });
     }
   });
 
 
   Meteor.startup(function () {
-    var conf = JSON.parse(Assets.getText("twitter.json"));
-    var Twit = Meteor.npmRequire('twit');
+    conf = JSON.parse(Assets.getText("twitter.json"));
+    Twit = Meteor.npmRequire('twit');
     nlp  = Meteor.npmRequire('nlp_compromise');
-    var T = new Twit({
-        consumer_key: conf.consumer.key,
-        consumer_secret: conf.consumer.secret,
-        access_token: conf.access_token.key,
-        access_token_secret: conf.access_token.secret
+
+    ServiceConfiguration.configurations.remove({
+      service: "twitter"
+    });
+    ServiceConfiguration.configurations.insert({
+      service: "twitter",
+      consumerKey: "TETpdGc8UUDTl6bzLpXhAtGVv",
+      loginStyle: "popup",
+      secret: "CkDDaaKielMfVokauIoO392p6etJydDJWa4k9ccgy6u54yBzeI"
     });
 
-    var stream =  T.stream("statuses/sample",
+/*
+    var stream =  T.stream("statuses/filter",
               {
-                "lang" : "en-gb"
+                "lang" : "en-gb",
+                "track": "facup"
               });
     stream.on("tweet", Meteor.bindEnvironment(function(data){
       Meteor.call("handleTweet", data);
-    }));
+    }));*/
   });
 }
